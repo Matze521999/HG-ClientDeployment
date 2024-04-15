c99e8eaa9a6196e9e921290b6903f2458dce6760
Matze521999 <119878942+Matze521999@users.noreply.github.com>
2024-04-15 13:51:55 +0200

    Update Install-WindowsUpdates.ps1

--- Install-WindowsUpdates.ps1
+++ Install-WindowsUpdates.ps1
@@ -6,16 +6,30 @@ Import-Module PSWindowsUpdate
 
 # Definiere eine Funktion zum Installieren von Updates und zur Fehlerbehandlung
 function Install-UpdatesWithRetry {
-    # Installiere Windows-Updates
-    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+    $retryCount = 3  # Anzahl der Versuche, das Update zu installieren
 
-    # Überprüfe auf Fehler bei der Installation
-    $lastInstallResult = Get-WindowsUpdateLog | Where-Object { $_.Message -like "*failed*" }
+    for ($i = 1; $i -le $retryCount; $i++) {
+        Write-Host "Versuch $i von $retryCount, Windows-Updates zu installieren..."
 
-    # Wenn ein Fehler aufgetreten ist, versuche erneut, das Update zu installieren
-    if ($lastInstallResult) {
-        Write-Host "Fehler bei der Installation festgestellt. Versuche, das Update erneut zu installieren..."
+        # Installiere Windows-Updates
         Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+
+        # Überprüfe auf Fehler bei der Installation
+        $lastInstallResult = Get-WindowsUpdateLog | Where-Object { $_.Message -like "*failed*" }
+
+        # Wenn kein Fehler aufgetreten ist, breche die Schleife ab
+        if (-not $lastInstallResult) {
+            Write-Host "Updates wurden erfolgreich installiert."
+            break
+        }
+
+        # Wenn ein Fehler aufgetreten ist und es nicht der letzte Versuch ist, warte eine Weile und versuche erneut
+        if ($i -lt $retryCount) {
+            Write-Host "Fehler bei der Installation festgestellt. Warte und versuche erneut..."
+            Start-Sleep -Seconds 30
+        } else {
+            Write-Host "Fehler bei der Installation festgestellt. Maximale Anzahl von Versuchen erreicht."
+        }
     }
 }
 
