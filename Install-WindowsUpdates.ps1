3a4199627ba31d207d6d31c5504161b9a4f69a10
Matze521999 <119878942+Matze521999@users.noreply.github.com>
2024-04-15 13:47:22 +0200

    Update Install-WindowsUpdates.ps1

--- Install-WindowsUpdates.ps1
+++ Install-WindowsUpdates.ps1
@@ -4,5 +4,20 @@ Install-Module -Name PSWindowsUpdate -Force
 # Importiere das PSWindowsUpdate-Modul
 Import-Module PSWindowsUpdate
 
-# Installiere Windows-Updates ohne Benutzereingriff
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+# Definiere eine Funktion zum Installieren von Updates und zur Fehlerbehandlung
+function Install-UpdatesWithRetry {
+    # Installiere Windows-Updates
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+
+    # Überprüfe auf Fehler bei der Installation
+    $lastInstallResult = Get-WindowsUpdateLog | Where-Object { $_.Message -like "*failed*" }
+
+    # Wenn ein Fehler aufgetreten ist, versuche erneut, das Update zu installieren
+    if ($lastInstallResult) {
+        Write-Host "Fehler bei der Installation festgestellt. Versuche, das Update erneut zu installieren..."
+        Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+    }
+}
+
+# Rufe die Funktion zum Installieren von Updates mit Fehlerbehandlung auf
+Install-UpdatesWithRetry
