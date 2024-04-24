d49e1ec2b35932310b571c6e5b7c85e27e4462e4
Matze521999 <119878942+Matze521999@users.noreply.github.com>
2024-04-24 10:59:31 +0200

    Update Install-WindowsUpdates.ps1

--- Install-WindowsUpdates.ps1
+++ Install-WindowsUpdates.ps1
@@ -4,4 +4,4 @@ Install-Module -Name PSWindowsUpdate -Force
 # Importiere das PSWindowsUpdate-Modul
 Import-Module PSWindowsUpdate
 
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll #-AutoReboot
