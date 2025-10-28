8481b5ced0d803a5e6c79e825f8286ffaab80860
Matze521999 <119878942+Matze521999@users.noreply.github.com>
2025-10-28 08:50:59 +0100

    Update OEM support information

--- Install-OEMInformationen.ps1
+++ Install-OEMInformationen.ps1
@@ -10,7 +10,7 @@ function Set-OEMInformation {
 
     $KeyNames = ("Manufacturer","SupportHours","SupportPhone","SupportURL","Line1","Logo")
     $KeyValues = @(
-        "Example Company",
+        "helpme!group - PC Help IT GmbH & Co. KG",
         "Montag bis Freitag, 08:00 - 17:00 Uhr",
         "+49 000 000000",
         "http://www.helpme.de",
